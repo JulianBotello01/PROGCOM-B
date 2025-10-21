804519ba12958f49b1e9f0c651080ab3ca1fa087
JulianBotello01 <jbotello118@unab.edu.co>
2025-10-21 18:05:06 -0500

    Add files via upload

---
+++ Ejercicios clase 21.10.2025.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        String nombres="Julián Santiago";
+        String apellidos="Botello Gracia";
+        //char p_letra="j";
+        System.out.println("Mi nombre es: "+nombres+" "+apellidos);
+        int edad=19;
+        double estatura=1.83;
+        System.out.println("Tengo "+edad+" años y mido "+estatura);
+        boolean mayor_edad=true;
+        System.out.println("Eso es todo amigos (no me cae bien la profe)");
+        System.out.println("(Mentiras (; ))");
+    }
+}
